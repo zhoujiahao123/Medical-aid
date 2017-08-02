e602486e58bce965626c8a32a3151a6bf63e9c8d
zhoujiahao <zhou.jiahao@foxmail.com>
2017-08-02 14:21:04 +0800

    s

--- lib1/src/main/java/com/example/MyClass.java
+++ lib1/src/main/java/com/example/MyClass.java
@@ -1,13 +1,37 @@
 package com.example;
 
 import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 public class MyClass {
     public static void main(String[] args) throws Exception {
-        Schema schema=new Schema(1,"com.example.greendao");
-
+        Schema schema=new Schema(1,"com.zxr.medicalaid");
+        addDate(schema);
+        addUser(schema);
+        addMedicalList(schema);
         //自动生成代码并且给予生成代码的路径
         new DaoGenerator().generateAll(schema,"app/src/main/java-gen");
+    }
+    public static void addUser(Schema schema){
+        Entity user = schema.addEntity("User");
+        user.addIdProperty().primaryKey();
+        user.addStringProperty("idString");
+        user.addStringProperty("uName");
+        user.addStringProperty("phoneNumber");
+        user.addStringProperty("password");
+        user.addStringProperty("type");
+        user.addIntProperty("isAlready");
+    }
+    public static void addMedicalList(Schema schema){
+        Entity list = schema.addEntity("MedicalList");
+        list.addStringProperty("date");
+        list.addStringProperty("name");
+        list.addStringProperty("weight");
+        list.addStringProperty("patient");
+    }
+    public static void addDate(Schema schema){
+        Entity date = schema.addEntity("Date");
+        date.addStringProperty("date");
     }
 }
